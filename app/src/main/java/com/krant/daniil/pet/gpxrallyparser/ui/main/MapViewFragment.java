f0bad24f534fa3b9ffe7118f4b61a1f1fa6a2d0b
Daniil.Krant <daniil.krant@globallogic.com>
2021-07-26 12:34:10 +0300

    Add tabbed activity, moved map to fragment
    Signed-off-by: Daniil.Krant <daniil.krant@globallogic.com>

--- app/src/main/java/com/krant/daniil/pet/gpxrallyparser/ui/main/MapViewFragment.java
+++ app/src/main/java/com/krant/daniil/pet/gpxrallyparser/ui/main/MapViewFragment.java
@@ -1,23 +1,31 @@
-package com.krant.daniil.pet.gpxrallyparser;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.krant.daniil.pet.gpxrallyparser.ui.main;
 
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.krant.daniil.pet.gpxrallyparser.GPXDataRoutine;
+import com.krant.daniil.pet.gpxrallyparser.LexicalProcessor;
+import com.krant.daniil.pet.gpxrallyparser.R;
+import com.krant.daniil.pet.gpxrallyparser.RallyPoint;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback,
+public class MapViewFragment extends Fragment implements OnMapReadyCallback,
         TextToSpeech.OnInitListener, GoogleMap.OnMarkerClickListener {
 
     private final static String TITLE_ID_DELIM = ": ";
@@ -28,18 +36,19 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     LexicalProcessor mLexicalProcessor;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
 
-        mGpxParser = new GPXDataRoutine(getApplicationContext());
-        mLexicalProcessor = new LexicalProcessor(getApplicationContext());
-        mTextToSpeech = new TextToSpeech(this, this);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        mGpxParser = new GPXDataRoutine(getContext());
+        mLexicalProcessor = new LexicalProcessor(getContext());
+        mTextToSpeech = new TextToSpeech(getContext(), this);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+        return rootView;
     }
 
     @Override
@@ -54,7 +63,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             mMap.setOnMarkerClickListener(this);
             mMap.moveCamera(CameraUpdateFactory.
                     newLatLngZoom(new LatLng(rallyPoints.get(0).getLatitude(),
-                    rallyPoints.get(0).getLongitude()), 20));
+                            rallyPoints.get(0).getLongitude()), 20));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -63,7 +72,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onInit(int i) {
         if (i == TextToSpeech.SUCCESS) {
-            int result = mTextToSpeech.setLanguage(Locale.UK);
+            int result = mTextToSpeech.setLanguage(Locale.getDefault());
             if (result == TextToSpeech.LANG_MISSING_DATA ||
                     result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 Log.e("error", "This Language is not supported");
